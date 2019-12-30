046d366672161eec42997987fc5873c64b10504a
Briyan <salampemenang2017@gmail.com>
2019-12-30 13:47:51 +0700

    viewDok

--- app/Dok.php
+++ app/Dok.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Dok extends Model
 {
     protected $table = 'dokumentasi';
+    protected $fillable = [	'id_sub_kegiatan',
+    						'nama_sub_kegiatan',
+    						'video_dokumentasi',
+    						'waktu_video_dokumentasi'
+    					];
     protected $guarded = ['id_dokumentasi'];
     protected $primaryKey = 'id_dokumentasi';
 }
--- app/Http/Controllers/DokController.php
+++ app/Http/Controllers/DokController.php
@@ -10,29 +10,35 @@ use Carbon\Carbon;
 
 class DokController extends Controller
 {
-    
+
     public function viewDok($id)
     { 
         $foto = DB::table('dokumentasi')
-                    ->join('foto', 'dokumentasi.id', '=', 'foto.dokumentasi_id')
-                    ->where('dokumentasi.id_sub_kegiatan', '=', $id)
-                    ->select('foto.foto_dokumentasi', 'foto.waktu_foto_dokumentasi')
-                    ->get();
+        ->join('foto', 'dokumentasi.id', '=', 'foto.dokumentasi_id')
+        ->where('dokumentasi.id_sub_kegiatan', '=', $id)
+        ->select('foto.foto_dokumentasi', 'foto.waktu_foto_dokumentasi')
+        ->get();
 
         $dok = DB::table('dokumentasi')
-                    ->where('dokumentasi.id_sub_kegiatan', '=', $id)
-                    ->select('dokumentasi.*')
-                    ->get();
+        ->where('dokumentasi.id_sub_kegiatan', '=', $id)
+        ->select('dokumentasi.*')
+        ->get();
 
         return view('View-Dokumentasi', ['foto' => $foto, 'dok' => $dok]);
     }
 
     public function sendDok($id_sub_kegiatan, $nama_sub_kegiatan)
     {   
-        $sendDok = new Dok;
-        $sendDok->id_sub_kegiatan = $id_sub_kegiatan;
-        $sendDok->nama_sub_kegiatan = $nama_sub_kegiatan;
-        $sendDok->save();
+        if(DB::table('dokumentasi')->where('id_sub_kegiatan', $id_sub_kegiatan)->first()){
+            return redirect('/view/dokumentasi/'.$id_sub_kegiatan);
+        }else{
+            $sendDok = new Dok;
+            $sendDok->id_sub_kegiatan = $id_sub_kegiatan;
+            $sendDok->nama_sub_kegiatan = $nama_sub_kegiatan;
+            $sendDok->save();
+
+            return redirect('/view/dokumentasi/'.$id_sub_kegiatan);
+        }
     }
 
 
